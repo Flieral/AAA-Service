55b32d4dc401ecf78e5e75e307c3e186cdc3eb27
MrWooJ <hurricanc@gmail.com>
2016-09-20 15:35:29 +0430

    Implement Register Checker Algorithm
    * Enhance Register Functionality

--- logic/account/register.js
+++ logic/account/register.js
@@ -1,69 +1,87 @@
 var utility			= require('../logic/utility')
 var redisClient		= require('../redis_client/redisClient')
-var configuration 	= require('../config/configuration.json');
+var configuration 	= require('../config/configuration.json')
 
 /*
 	payload = {
-		Email: emailX									-> Required, Any Arbitary UTF8 String
-		CompanyName: companyNameX						-> Required, Any Arbitary UTF8 String
-		Username: usernameX								-> Required, Any Arbitary UTF8 String
-		Password: passwordX								-> Required, Any Arbitary UTF8 String
-		RegistrationCountry: SampleX					-> Required, Based on config/configuration JSON file
-		RegistrationMacAddress: registerMacAddressX		-> Required, Any Arbitary UTF8 String
+		IPAddress: 											-> Required, Any Arbitary UTF8 String
+		ReferralAccount:									-> Required, Any Arbitary UTF8 String
+		AccountModel: {
+			Email: emailX									-> Required, Any Arbitary UTF8 String
+			CompanyName: companyNameX						-> Required, Any Arbitary UTF8 String
+			Username: usernameX								-> Required, Any Arbitary UTF8 String
+			Password: passwordX								-> Required, Any Arbitary UTF8 String
+			RegistrationCountry: SampleX					-> Required, Based on config/configuration JSON file
+			RegistrationMacAddress: registerMacAddressX		-> Required, Any Arbitary UTF8 String
+		},
+		NetworkModel: {
+			ISP:											-> Required, Any Arbitary UTF8 String
+			Agent:											-> Required, Any Arbitary UTF8 String
+		}
 	}
 */
 module.exports = function(userToken, payload, callback)
 {
 	var tableName
-	var errorCheck
+	var userSessionToken
+	var sessionLength = 1000
 	var valueTime = payload[configuration.ConstantMMTime]
-	var accountHashID = utility.generateUniqueHashID()
+	var accountHashID = payload.CompanyName + "-" + payload.Username
 
-	/* Add to MonitorModel:MonitorHashID */
-	tableName 	= configuration.TableMAMonitorModel + monitorHashID
-	redisClient.hashModel.createModel(userToken, tableName, payload, function(error, result){})
+	payload.AccountModel.RegistrationStatus = configuration.Enum.Sample1
+	payload.AccountModel.SuspendStatus = configuration.Enum.Sample1
+	payload.AccountModel.WebhookBaseURL = payload.AccountModel.CompanyName
+	payload.AccountModel.Attempt = 0
 
-	/* Add to AccountModel:MonitorModel:AccountHashID */
-	tableName 	= configuration.TableMSAccountModelMonitorModel + accountHashID
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	payload.NetworkModel.Country = payload.AccountModel.RegistrationCountry
+	payload.NetworkModel.MacAddress = payload.AccountModel.RegistrationMacAddress
 
-	/* Add to MonitorModel:StatusCodeType1:AccountHashID */
-	tableName 	= configuration.TableMonitorModel.StatusCode[payload[configuration.ConstantMMStatusCode]] + accountHashID
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to AccountModel:accountHashID */
+	tableName 	= configuration.TableMAAccountModel + accountHashID
+	redisClient.hashModel.createModel(userToken, tableName, payload.AccountModel, function(error, result){})
 
-	/* Add to MonitorModel:ActionType1:AccountHashID */
-	tableName 	= configuration.TableMonitorModel.Action[payload[configuration.ConstantMMAction]] + accountHashID
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to NetworkPrivacy:IPDetails:IPAddress */
+	tableName 	= configuration.TableMANetworkPrivacyIPDetails + payload.IPAddress
+	redisClient.hashModel.createModel(userToken, tableName, payload.NetworkModel, function(error, result){})
 
-	/* Add to MonitorModel:ServiceCallerType1:AccountHashID */
-	tableName 	= configuration.TableMonitorModel.ServiceCaller[payload[configuration.ConstantMMServiceCaller]] + accountHashID
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to AccountModel:ReferralPlan:AccountHashID */
+	tableName 	= configuration.TableMSAccountModelReferralPlan + payload.ReferralAccount
+	redisClient.zSetModel.createKeyModel(userToken, tableName, accountHashID, valueTime, function(error, result){})
 
-	/* Add to MonitorModel:ModuleCallerType1:AccountHashID */
-	tableName 	= configuration.TableMonitorModel.ModuleCaller[payload[configuration.ConstantMMModuleCaller]] + accountHashID
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to NetworkPrivacy:IPList:AccountHashID */
+	tableName 	= configuration.TableMSNetworkPrivacyIPList + accountHashID
+	redisClient.zSetModel.createKeyModel(userToken, tableName, payload.IPAddress, valueTime, function(error, result){})
 
-	/* Add to MonitorModel: */
-	tableName 	= configuration.TableMLMonitorModel
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to AccountModel:CompanyName:companyName */
+	tableName 	= configuration.TableMSAccountModelCompanyName + payload.AccountModel.CompanyName
+	redisClient.stringModel.createModel(userToken, tableName, accountHashID, function(error, result){})
 
-	/* Add to MonitorModel:StatusCodeType1: */
-	tableName 	= configuration.TableMonitorModel.StatusCode[payload[configuration.ConstantMMStatusCode]]
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to AccountModel:Username:username */
+	tableName 	= configuration.TableMSAccountModelUsername + payload.AccountModel.Username
+	redisClient.stringModel.createModel(userToken, tableName, accountHashID, function(error, result){})
 
-	/* Add to MonitorModel:ActionType1: */
-	tableName 	= configuration.TableMonitorModel.Action[payload[configuration.ConstantMMAction]]
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to AccountModel:Email:email */
+	tableName 	= configuration.TableMSAccountModelEmail + payload.AccountModel.Email
+	redisClient.stringModel.createModel(userToken, tableName, accountHashID, function(error, result){})
 
-	/* Add to MonitorModel:ServiceCallerType1: */
-	tableName 	= configuration.TableMonitorModel.ServiceCaller[payload[configuration.ConstantMMServiceCaller]]
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to AccountModel:RegistrationStatusType1: */
+	tableName 	= configuration.TableAccountModel.RegistrationStatus[payload[configuration.ConstantAMRegistrationStatus]]
+	redisClient.zSetModel.createKeyModel(userToken, tableName, accountHashID, valueTime, function(error, result){})
 
-	/* Add to MonitorModel:ModuleCallerType1: */
-	tableName 	= configuration.TableMonitorModel.ModuleCaller[payload[configuration.ConstantMMModuleCaller]]
-	redisClient.zSetModel.createKeyModel(userToken, tableName, monitorHashID, valueTime, function(error, result){})
+	/* Add to AccountModel:SuspendStatusType1: */
+	tableName 	= configuration.TableAccountModel.SuspendStatus[payload[configuration.ConstantAMSuspendStatus]]
+	redisClient.zSetModel.createKeyModel(userToken, tableName, accountHashID, valueTime, function(error, result){})
+
+	/* Add to AccountModel:CountryType1: */
+	tableName 	= configuration.TableAccountModel.RegistrationCountry[payload[configuration.ConstantAMRegistrationCountry]]
+	redisClient.zSetModel.createKeyModel(userToken, tableName, accountHashID, valueTime, function(error, result){})
+
+	/* Add to AccountModel:UserToken:accountHashID */
+	tableName 	= configuration.TableMAAccountModelUserToken + accountHashID
+	userSessionToken = utility.generateUniqueHashID()
+	redisClient.stringModel.createTemporaryModel(userToken, tableName, userSessionToken, sessionLength, function(error, result){})
 
 	var result = {}
-	result.result =	monitorHashID
+	result.result =	userSessionToken
 	callback(null, result)
 }
