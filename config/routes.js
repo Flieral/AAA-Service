6dc1898ac8c9aae8ce7e6455a84ccd905f4c3b8f
MrWooJ <hurricanc@gmail.com>
2016-12-10 15:28:37 +0330

    Add Default Main Routes for WebServer API

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,21 @@
-exports.default =
-{
-	routes: function(api)
-	{
+exports.default = {
+	routes: function(api) {
 		return {
-			get:			[
-				/* 	STATUS 	*/
+			get:[
+				{path: '/account/login'											, action: 'login'},
 				{path: '/:apiVersion/Service/Status'				, action: 'Status'}
+			],
+			put:[
+				{path: '/account/changePassword'						, action: 'changePassword'}
+			],
+			post:[
+				{path: '/account/register'									, action: 'register'},
+				{path: '/account/confirm'										, action: 'confirm'},
+				{path: '/account/validate'									, action: 'validate'}
+			],
+			delete:[
+
 			]
-		};
+		}
 	}
-};
+}
