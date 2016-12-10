b54c4d97347f631424446198cd20f0d8079671a7
mohammad4x <m.ferdosi94@gmail.com>
2016-12-10 23:43:56 +0330

    Implement Confirm Rest API in AAA-Service

---
+++ actions/confirm.js
@@ -0,0 +1,27 @@
+var confirmAction = require('../logic/account/confirmAction')
+
+var Input = {
+  accountHashID: {
+    required: true,
+    formatter: function(param, connection, actionTemplate) {
+      return JSON.parse(new Buffer(param, 'base64'))
+    }
+  }
+}
+
+exports.confirm = {
+  name: 'confirm',
+  description: 'confirm a pending User',
+  input: Input,
+
+  run: function(api, data, next) {
+    confirmAction.confirm(data.params.accountHashID, function (error, replies) {
+      if (error) {
+        data.response.error = error.error
+        next(error)
+      }
+      data.response.result = replies
+    })
+  }
+}
+
