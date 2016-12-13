ab470c34552b299c7920c3ec12b14c9ba38f0712
MrWooJ <hurricanc@gmail.com>
2016-12-13 13:44:48 +0330

    Implement Session Manager for Renew/Terminate Sessions Operations

---
+++ logic/account/sessionManager.js
@@ -0,0 +1,24 @@
+var redisClient   = require('../redis_client/redisClient').getClient()
+var configuration = require('../config/configuration.json')
+var utility       = require('../../public/utility')
+
+module.exports = {
+  renewSessionForAccount: function(accountHashID, callback) {
+    var sessionTable = configuration.TableMAAccountModelUserToken + accountHashID
+    var sessionHashID = utility.generateUniqueHashID()
+    redisClient.set(sessionTable, sessionHashID, 'EX', configuration.MaximumSessionLength, 'NX', function(err, replies) {
+      if (err)
+      callback(err, null)
+      callback(null, configuration.message.session.renew)
+    }
+  },
+
+  terminateSessionForAccount: function(accountHashID, callback) {
+    var sessionTable = configuration.TableMAAccountModelUserToken + accountHashID
+    redisClient.pexpire(sessionTable, 10, function(err, replies) {
+      if (err)
+      callback(err, null)
+      callback(null, configuration.message.session.terminate)
+    })
+  }
+}
