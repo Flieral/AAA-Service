6738791700c29654e8e1c341614fd23abb5a9aa1
MrWooJ <hurricanc@gmail.com>
2016-12-13 13:53:35 +0330

    Add Starter for Login Module to Handle All Situations
    * Enhance Functionality and Also Fix Strategy
    * Move RenewSession Function to SessionManager Module
    * Get AccountHashID with Preferred Previous Modules Functions

--- logic/account/loginAction.js
+++ logic/account/loginAction.js
@@ -1,59 +1,66 @@
 var redisClient   = require('../redis_client/redisClient').getClient()
 var configuration = require('../config/configuration.json')
 var utility       = require('../../public/utility')
+var userChecker   = require('../logic/account/userChecker')
+var attemptChecker= require('./attemptChecker')
 
 module.exports = {
+  login: function(loginObject, callback) {
+    if (loginObject.accountModel.option === 'email')
+    this.loginByEmail(loginObject.accountModel.user, loginObject.accountModel.password, function(err, replies) {
+      if (err)
+      callback(err, null)
+      callback(null, replies)
+    })
+    else if (loginObject.accountModel.option === 'username')
+    this.loginByUsername(loginObject.accountModel.user, loginObject.accountModel.password, function(err, replies) {
+      if (err)
+      callback(err, null)
+      callback(null, replies)
+    })
+  },
+
   loginByUsername: function(username, password, callback) {
-    var userTable = configuration.TableMSAccountModelUsername + username
-		redisClient.get(userTable, function (err, replies) {
-    	if (err)
-				callback(err, null)
+    userChecker.getAccountIdentifierByUsername(username, function(err, replies) {
+      if (err)
+      callback(err, null)
       if (replies === 'null')
-        callback(null, configuration.message.login.notExistsUser)
-      else
-        this.passwordCheck(replies, password, function(err, replies) {
-          if (err)
-            callback(err, null)
-          callback(null, replies)
-        })
-		})
+      callback(new Error(configuration.message.user.notExistUser), null)
+      this.passwordCheck(replies, password, function(err, replies) {
+        if (err)
+        callback(err, null)
+        callback(null, result)
+      })
+    })
   },
 
   loginByEmail: function(email, password, callback) {
-    var emailTable = configuration.TableMSAccountModelEmail + email
-		redisClient.get(emailTable, function (err, replies) {
-    	if (err)
-				callback(err, null)
-			if (replies === 'null')
-        callback(null, configuration.message.login.notExistsUser)
-      else
-        this.passwordCheck(replies, password, function(err, replies) {
-          if (err)
-            callback(err, null)
-          callback(null, replies)
-        })
-		})
+    userChecker.getAccountIdentifierByEmail(email, function(err, replies) {
+      if (err)
+      callback(err, null)
+      if (replies === 'null')
+      callback(new Error(configuration.message.user.notExistUser), null)
+      this.passwordCheck(replies, password, function(err, replies) {
+        if (err)
+        callback(err, null)
+        callback(null, result)
+      })
+    })
   },
 
   passwordCheck: function(accountHashID, password, callback) {
     var modelTable = configuration.TableMAAccountModel + accountHashID
     redisClient.hget(modelTable, configuration.ConstantAMPassword, function(err, replies) {
       if (err)
-        callback(err, null)
+      callback(err, null)
       if (replies === password)
-        callback(null, configuration.message.login.successfulLogin)
+      callback(null, configuration.message.login.successfulLogin)
       else
-        callback(null, configuration.message.login.failedLogin)
-    }
-  },
-
-  renewSessionForAccount: function(accountHashID, callback) {
-    var sessionTable = configuration.TableMAAccountModelUserToken + accountHashID
-    var sessionHashID = utility.generateUniqueHashID()
-    redisClient.set(sessionTable, sessionHashID, 'EX', configuration.MaximumSessionLength, 'NX', function(err, replies) {
-      if (err)
+      attemptChecker.incrementUserAttempt(accountHashID, function(err, replies) {
+        if (err)
         callback(err, null)
-      callback(null, configuration.message.login.renewSession)
+        callback(null, replies)
+      })
     }
   }
 }
