6fcec8fbf09ab96a0f395c2c9554146f9ab2360b
MrWooJ <hurricanc@gmail.com>
2016-12-13 13:46:32 +0330

    Add 3 Functions for Getting AccountHashID by Username/Email/CompanyName
    * Also Using this Functions in Checking Account Existence

--- logic/account/userChecker.js
+++ logic/account/userChecker.js
@@ -2,30 +2,81 @@ var redisClient   = require('../redis_client/redisClient').getClient()
 var configuration = require('../config/configuration.json')
 
 module.exports = {
-	checkUserExistenceByEmail: function(email, callback) {
-		var emailTable = configuration.TableMSAccountModelEmail + email
-		redisClient.get(emailTable, function (err, replies) {
-    	if (err)
+	startUserChecking: function(accountModelObject, callback){
+		this.checkUserExistenceByUsername(accountModelObject.accountModel.username, function(err, replies) {
+			if (err)
 				callback(err, null)
-			callback(null, replies)
+			else
+				this.checkUserExistenceByEmail(accountModelObject.accountModel.email, function(err, replies) {
+					if (err)
+						callback(err, null)
+					else
+						this.checkUserExistenceByCompanyName(accountModelObject.accountModel.companyName, function(err, replies) {
+							if (err)
+								callback(err, null)
+							callback(null, replies)
+						})
+				})
+		}
+	},
+
+	checkUserExistenceByEmail: function(email, callback) {
+		this.getAccountIdentifierByEmail(email, function(err, replies) {
+			if (err)
+				callback(err, null)
+			if (replies === 'null')
+				callback(null, configuration.message.user.notExistUser)
+			else
+				callback(new Error(configuration.message.user.existUser), null)
 		})
 	},
 
 	checkUserExistenceByUsername: function(username, callback) {
-		var userTable = configuration.TableMSAccountModelUsername + username
-		redisClient.get(userTable, function (err, replies) {
-    	if (err)
+		this.getAccountIdentifierByUsername(username, function(err, replies) {
+			if (err)
 				callback(err, null)
-			callback(null, replies)
+			if (replies === 'null')
+				callback(null, configuration.message.user.notExistUser)
+			else
+				callback(new Error(configuration.message.user.existUser), null)
 		})
 	},
 
 	checkUserExistenceByCompanyName: function(companyName, callback) {
-		var companyTable = configuration.TableMSAccountModelCompanyName + companyName
-		redisClient.get(companyTable, function (err, replies) {
-    	if (err)
+		this.getAccountIdentifierByCompanyName(companyName, function(err, replies) {
+			if (err)
+				callback(err, null)
+			if (replies === 'null')
+				callback(null, configuration.message.user.notExistUser)
+			else
+				callback(new Error(configuration.message.user.existUser), null)
+		})
+	},
+
+	getAccountIdentifierByEmail: function(email, callback) {
+		var emailTable = configuration.TableMSAccountModelEmail + email
+		redisClient.get(emailTable, function (err, replies) {
+			if (err)
 				callback(err, null)
 			callback(null, replies)
-		})
+		}
+	},
+
+	getAccountIdentifierByUsername: function(username, callback) {
+		var userTable = configuration.TableMSAccountModelUsername + username
+		redisClient.get(userTable, function (err, replies) {
+			if (err)
+				callback(err, null)
+			callback(null, replies)
+		}
+	},
+
+	getAccountIdentifierByCompanyName: function(companyName, callback) {
+		var companyTable = configuration.TableMSAccountModelCompanyName + companyName
+		redisClient.get(companyTable, function (err, replies) {
+			if (err)
+				callback(err, null)
+			callback(null, replies)
+		}
 	}
 }
