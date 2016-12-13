cb1797a67755da0dff013a064a614da358a1c2eb
MrWooJ <hurricanc@gmail.com>
2016-12-13 13:51:24 +0330

    Re-Implement Check Password Function with New Style
    * Re-Indent this Module Included

--- logic/account/changePasswordAction.js
+++ logic/account/changePasswordAction.js
@@ -1,42 +1,59 @@
 var redisClient   = require('../redis_client/redisClient').getClient()
 var configuration = require('../config/configuration.json')
 var userChecker = require('./userChcker')
+
 module.exports = {
   startPasswordChanging: function(accountModelObject, callback) {
     var accountHashID
     if (accountModelObject.option === 'username')
     {
-      userChecker.checkUserExistenceByUsername(accountModel.user, function (err, replies) {
+      userChecker.getAccountIdentifierByUsername(accountModel.user, function (err, replies) {
         if (err)
-          callback(err, null)
+        callback(err, null)
         accountHashID = replies
       }
     }
     else if (accountModelObject.option === 'email')
     {
-      userChecker.checkUserExistenceByEmail(accountModel.user, function (err, replies) {
+      userChecker.getAccountIdentifierByEmail(accountModel.user, function (err, replies) {
         if (err)
-          callback(err, null)
+        callback(err, null)
         accountHashID = replies
       }
     }
+
+    this.checkPassword(accountHashID, accountModelObject.password, function(err, replies) {
+      if (err)
+      callback(err, null)
+      passwordChange(accountHashID, accountModelObject.newPassword, function(err, replies) {
+        if (err)
+        callback(err, null)
+        callback(null, configuration.message.changePassword.successful)
+      })
+    })
+  },
+
+  passwordCheck: function(accountHashID, password, callback) {
     var modelTable = configuration.TableMAAccountModel + accountHashID
     redisClient.hget(modelTable, configuration.ConstantAMPassword, function(err, replies) {
       if (err)
+      callback(err, null)
+      if (replies === password)
+      callback(null, configuration.message.changePassword.equalPassword)
+      else
+      attemptChecker.incrementUserAttempt(accountHashID, function(err, replies) {
+        if (err)
         callback(err, null)
-      if (replies === accountModelObject.password)
-        passwordChange(accountHashID, accountModelObject.newPassword, function(err, callback) {
-          if (err)
-            callback(err, null)
-          callback(null, 'password succesfully changed message')
+        callback(null, configuration.message.changePassword.wrongPassword)
       })
     }
-  }
+  },
+
   passwordChange: function(accountHashID, Password, callback) {
     var modelTable = configuration.TableMAAccountModel + accountHashID
     redisClient.hset(modelTable, configuration.ConstantAMPassword, function(err, replies) {
       if (err)
-        callback(err, null)
+      callback(err, null)
     })
   }
 }
