b6cea080657c7cebb73fbde93e6945ac8e9f44f7
MrWooJ <hurricanc@gmail.com>
2016-12-13 13:54:53 +0330

    Add ChangeSuspensionType for Specific Account
    * Fix Remove From Mentioned SuspensionList
    * Enhance Return Type Messages

--- logic/account/suspensionChecker.js
+++ logic/account/suspensionChecker.js
@@ -12,33 +12,45 @@ module.exports = {
     multi.hset(modelTable, configuration.ConstantAMSuspendStatus, suspendType)
     multi.exec(function (err, replies) {
       if (err)
-        callback(err, null)
-      callback(null, configuration.message.userSuspend)
+      callback(err, null)
+      callback(new Error(configuration.message.user.suspend), null)
     })
   },
 
-  removeFromSuspensionList: function(accountHashID, callback) {
-    var suspendTable = TableAccountModel.SuspendStatus[configuration.Enum.SuspendType.None]
+  removeFromSuspensionList: function(accountHashID, suspendType, callback) {
+    var suspendTable = TableAccountModel.SuspendStatus[suspendType]
     var modelTable = configuration.TableMAAccountModel + accountHashID
     var multi = redisClient.multi()
     multi.zrem(suspendTable, accountHashID)
     multi.hset(modelTable, configuration.ConstantAMSuspendStatus, configuration.Enum.SuspendType.None)
     multi.exec(function (err, replies) {
       if (err)
-        callback(err, null)
-      callback(null, configuration.message.userSafe)
+      callback(err, null)
+      callback(null, configuration.message.user.safe)
     })
   },
+
+  changeSuspensionType: function(accountHashID, oldSuspendType, newSuspendType, callback) {
+    this.removeFromSuspensionList(accountHashID, oldSuspendType, function(err, replies) {
+      if (err)
+      callback(err, null)
+      this.addToSuspensionList(accountHashID, newSuspendType, function(err, replies) {
+        if (err)
+        callback(err, null)
+        callback(null, replies)
+      })
+    })
+  }
 
   checkAccountSuspension: function(accountHashID, callback) {
     var modelTable = configuration.TableMAAccountModel + accountHashID
     redisClient.hget(modelTable, configuration.ConstantAMSuspendStatus, function(err, replies) {
       if (err)
-        callback(err, null)
+      callback(err, null)
       if (replies === configuration.Enum.SuspendType.None)
-        callback(null, configuration.message.userSafe)
+      callback(null, configuration.message.user.safe)
       else
-        callback(null, configuration.message.userSuspend)
+      callback(new Error(configuration.message.user.suspend), null)
     }
   }
 }
