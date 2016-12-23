3ed686d7a5bee737ebafc2ea77a867570a716fea
MrWooJ <hurricanc@gmail.com>
2016-12-23 12:04:40 +0330

    Use of 'Else' for Stop Executing After Occuring Error

--- actions/register.js
+++ actions/register.js
@@ -1,17 +1,17 @@
-var registerAction  = require('../logic/account/registerAction')
-var userChecker     = require('../logic/account/userChecker')
+var registerAction = require('../logic/account/registerAction')
+var userChecker = require('../logic/account/userChecker')
 
 exports.register = {
   name: "register",
   description: "Register a User",
 
-  run: function(api, data, next) {
+  run: function (api, data, next) {
     var payload = JSON.parse(JSON.stringify(data.connection.rawConnection.params.body))
-      userChecker.startUserChecking(api.redisClient, payload.accountModel, function(err, result) {
-        if (err) {
-          data.response.error = err.error
-          next(err)
-        }
+    userChecker.startUserChecking(api.redisClient, payload.accountModel, function (err, result) {
+      if (err) {
+        data.response.error = err.error
+        next(err)
+      } else {
         registerAction.register(api.redisClient, payload, function (err, replies) {
           if (err) {
             data.response.error = err.error
@@ -20,6 +20,7 @@ exports.register = {
           data.response.result = replies
           next()
         })
-      })
+      }
+    })
   }
 }
