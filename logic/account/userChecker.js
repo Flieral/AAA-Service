e4612cf6693526d52f4d2c749f0d1dc96ad8d5df
MrWooJ <hurricanc@gmail.com>
2016-12-23 12:09:35 +0330

    Fix Problem of Executing Codes after Occuring Error

--- logic/account/userChecker.js
+++ logic/account/userChecker.js
@@ -1,81 +1,97 @@
 var configuration = require('../../config/configuration.json')
 
 module.exports = {
-	startUserChecking: function(redisClient, accountModelObject, callback){
-		this.checkUserExistenceByUsername(redisClient, accountModelObject.username, function(err, replies) {
-			if (err)
+	startUserChecking: function (redisClient, accountModelObject, callback) {
+		module.exports.checkUserExistenceByUsername(redisClient, accountModelObject.username, function (err, replies) {
+			if (err) {
 				callback(err, null)
-			else
-				this.checkUserExistenceByEmail(redisClient, accountModelObject.email, function(err, replies) {
-					if (err)
+				return
+			}
+			module.exports.checkUserExistenceByEmail(redisClient, accountModelObject.email, function (err, replies) {
+				if (err) {
+					callback(err, null)
+					return
+				}
+				module.exports.checkUserExistenceByCompanyName(redisClient, accountModelObject.companyName, function (err, replies) {
+					if (err) {
 						callback(err, null)
-					else
-						this.checkUserExistenceByCompanyName(redisClient, accountModelObject.companyName, function(err, replies) {
-							if (err)
-								callback(err, null)
-							callback(null, replies)
-						})
+						return
+					}
+					callback(null, replies)
 				})
+			})
 		})
 	},
 
-	checkUserExistenceByEmail: function(redisClient, email, callback) {
-		this.getAccountIdentifierByEmail(redisClient, email, function(err, replies) {
-			if (err)
+	checkUserExistenceByEmail: function (redisClient, email, callback) {
+		this.getAccountIdentifierByEmail(redisClient, email, function (err, replies) {
+			if (err) {
 				callback(err, null)
-			if (replies === 'null')
+				return
+			}
+			if (replies == null || replies == undefined)
 				callback(null, configuration.message.account.notExist)
 			else
-				callback(new Error(configuration.message.account.exist), null)
+				callback(new Error(configuration.message.account.exists), null)
 		})
 	},
 
-	checkUserExistenceByUsername: function(redisClient, username, callback) {
-		this.getAccountIdentifierByUsername(redisClient, username, function(err, replies) {
-			if (err)
+	checkUserExistenceByUsername: function (redisClient, username, callback) {
+		this.getAccountIdentifierByUsername(redisClient, username, function (err, replies) {
+			if (err) {
 				callback(err, null)
-			if (replies === 'null')
+				return
+			}
+			if (replies == null || replies == undefined)
 				callback(null, configuration.message.account.notExist)
 			else
-				callback(new Error(configuration.message.account.exist), null)
+				callback(new Error(configuration.message.account.exists), null)
 		})
 	},
 
-	checkUserExistenceByCompanyName: function(redisClient, companyName, callback) {
-		this.getAccountIdentifierByCompanyName(redisClient, companyName, function(err, replies) {
-			if (err)
+	checkUserExistenceByCompanyName: function (redisClient, companyName, callback) {
+		this.getAccountIdentifierByCompanyName(redisClient, companyName, function (err, replies) {
+			if (err) {
 				callback(err, null)
-			if (replies === 'null')
+				return
+			}
+			if (replies == null || replies == undefined)
 				callback(null, configuration.message.account.notExist)
 			else
-				callback(new Error(configuration.message.account.exist), null)
+				callback(new Error(configuration.message.account.exists), null)
 		})
 	},
 
-	getAccountIdentifierByEmail: function(redisClient, email, callback) {
+	getAccountIdentifierByEmail: function (redisClient, email, callback) {
 		var emailTable = configuration.TableMSAccountModelEmail + email
 		redisClient.get(emailTable, function (err, replies) {
-			if (err)
+			if (err) {
 				callback(err, null)
+				return
+			}
 			callback(null, replies)
 		})
 	},
 
-	getAccountIdentifierByUsername: function(redisClient, username, callback) {
+	getAccountIdentifierByUsername: function (redisClient, username, callback) {
 		var userTable = configuration.TableMSAccountModelUsername + username
 		redisClient.get(userTable, function (err, replies) {
-			if (err)
+			if (err) {
 				callback(err, null)
+				return
+			}
 			callback(null, replies)
 		})
 	},
 
-	getAccountIdentifierByCompanyName: function(redisClient, companyName, callback) {
+	getAccountIdentifierByCompanyName: function (redisClient, companyName, callback) {
 		var companyTable = configuration.TableMSAccountModelCompanyName + companyName
 		redisClient.get(companyTable, function (err, replies) {
-			if (err)
+			if (err) {
 				callback(err, null)
+				return
+			}
 			callback(null, replies)
 		})
-	}	
+	}
 }
